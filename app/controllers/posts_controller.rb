3a38885c9c70783e5727b92fe7480b38c65d5ee2
Maximiliano Bernard <bmaximiliano0210@gmail.com>
2024-04-23 19:39:44 -0300

    Add Index page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,8 +7,16 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.new(post_params)
-    @post.save
-    redirect_to @post
+    @post.user = current_user
+    if @post.save
+      redirect_to posts_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def index
+    @posts = Post.all
   end
 
   private
